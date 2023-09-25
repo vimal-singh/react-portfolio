e7db40ddb5c09912efbbf25023870caa025f060e
Vimal Singh <vimal.singh@non.keysight.com>
2023-09-25 19:58:57 +0530

    Works section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import Intro from "./components/Intro/intro";
 import Navbar from "./components/Navbar/navbar";
+import Skills from "./components/Skills/skills";
+import Works from "./components/Works/works";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Intro />
+      <Skills />
+      <Works />
     </div>
   );
 }
